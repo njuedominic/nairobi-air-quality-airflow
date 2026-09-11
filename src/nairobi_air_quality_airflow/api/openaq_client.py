5e164e0f58faadb12cf22a348b3cb00d94cca15f
njuedominic <njuemugodominic@gmail.com>
2026-09-11 10:37:37 +0300

    fix: update rate limit and retries

--- src/nairobi_air_quality_airflow/api/openaq_client.py
+++ src/nairobi_air_quality_airflow/api/openaq_client.py
@@ -1,11 +1,23 @@
 import requests
+from urllib3 import Retry
+from requests.adapters import HTTPAdapter
 
 
 class OpenAQClient:
     def __init__(self, api_key: str, host: str):
         self.base_url = f"https://{host}/v3"
 
+        retry_strategy = Retry(
+            total=5,
+            backoff_factor=1,
+            status_forcelist=[429, 500, 502, 503, 504],
+            allowed_methods=["HEAD", "GET", "OPTIONS"],
+            respect_retry_after_header=True,
+        )
+
+        adapter = HTTPAdapter(max_retries=retry_strategy)
         self.session = requests.Session()
+        self.session.mount("https://", adapter)
 
         self.session.headers.update(
             {
@@ -54,4 +66,25 @@ class OpenAQClient:
         payload = response.json()
 
         return payload.get("results", [])
-        
+
+    #----------------------------------------------------------------------------
+
+    def get_measurements(
+            self,
+            sensor_id: int,
+            limit: int = 100,
+    )-> list[dict]:
+
+        response = self.session.get(
+            f"{self.base_url}/sensors/{sensor_id}/measurements",
+            params={
+                "limit": limit,
+            },
+            timeout=30,
+        )
+
+        response.raise_for_status()
+
+        payload = response.json()
+
+        return payload.get("results", [])
